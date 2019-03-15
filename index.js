09810d3c837d42d286b9de2dd7e8cd79a6a171e0
Terry Tilley <terry@terrytilley.com>
2019-03-15 16:57:32 +0000

    scrape twitter and instagram for follower count

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Qwerty');
+import {
+  getHTML,
+  getTwitterFollowers,
+  getInstagramFollowers,
+} from './lib/scraper';
+
+async function init() {
+  const [twitterHTML, instagramHTML] = await Promise.all([
+    getHTML('https://twitter.com/wesbos/'),
+    getHTML('https://www.instagram.com/wesbos/'),
+  ]);
+
+  const twitterFollowers = await getTwitterFollowers(twitterHTML);
+  const instagramFollowers = await getInstagramFollowers(instagramHTML);
+
+  console.log(`You have ${twitterFollowers} followers on Twitter.`);
+  console.log(`You have ${instagramFollowers} followers on Instagram.`);
+}
+
+init();
